0b9d2871f2de1bdeef0719c81efae71f2a720498
Julia Chifman <42590316+chifman@users.noreply.github.com>
2021-08-20 12:40:06 -0400

    updates

---
+++ Rscript/local_activation_functions.R
@@ -0,0 +1,24 @@
+local_activation_function_1<-function(x2, x8, x3, x4, x23){min(max(x2,x8),min(min(not(x3),not(x4)),not(x23)))}
+local_activation_function_2<-function(x5, x6, x19){max(max((x5^2)%%3,x6),x19)}
+local_activation_function_3<-function(x5, x6, x7){min(min((2+2*x5^2)%%3,not(x6)),not(x7))}
+local_activation_function_4<-function(x5,x6){min((2+2*x5^2)%%3,not(x6))}
+local_activation_function_5<-function(x1){not(x1)}
+local_activation_function_6<-function(x1,x19){max(not(x1),x19)}
+local_activation_function_7<-function(x15){x15}
+local_activation_function_8<-function(x10,x13){max(x10,x13)}
+local_activation_function_9<-function(x10,x22){min(not(x10),x22)}
+local_activation_function_10<-function(x8,x9){min(not(x8),x9)}
+local_activation_function_11<-function(x1, x16, x21, x12){min(max(max(x1,x16),x21),not(x12))}
+local_activation_function_12<-function(x13){x13}
+local_activation_function_13<-function(x14, x16, x18){max(not(x14),max(x16,x18))}
+local_activation_function_14<-function(x11,x13){min(not(x11),(1+x13+2*x13^2)%%3)}
+local_activation_function_15<-function(x8,x11){max(not(x8),x11)}
+local_activation_function_16<-function(x15, x17, x20){min(max(x15,x17),not(x20))}
+local_activation_function_17<-function(x18,x21){max(not(x18),x21)}
+local_activation_function_18<-function(x16){x16}
+local_activation_function_19<-function(x18){x18}
+local_activation_function_20<-function(x21){x21}
+local_activation_function_21<-function(x11){x11}
+local_activation_function_22<-function(x23, x10, x24){min(x23,min(not(x10),not(x24)))}
+local_activation_function_23<-function(x22){not(x22)}
+local_activation_function_24<-function(x22){x22}
--- Rscript/script_to_write_r_functions_for_the_model.R
+++ Rscript/script_to_write_r_functions_for_the_model.R
@@ -1,5 +1,9 @@
 # Read dynamical system (in this case iron model) with 24 lines. 
-# Each line corresponds to a local activation function
+# Each line corresponds to a local activation function written in order,
+# e.g., line 1 corresponds to variable x1, line 2 to x2, etc.
+
+library(stringr)
+
 model<-readLines("./Models/iron_24_node.txt")
 
 # create an empty vector that will be used to write an R function to be used later for distance computations.
@@ -23,5 +27,6 @@ for(i in 1:length(model))
 	}
 }
 
-# save to table R functions
-write.table(model_out, file="local_activation_functions.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
+# save to table R functions with the extension to .R
+# this file then can be used with the second R script nonexpanding_check.R
+write.table(model_out, file="./Rscript/local_activation_functions.R", quote=FALSE, row.names=FALSE, col.names=FALSE)
